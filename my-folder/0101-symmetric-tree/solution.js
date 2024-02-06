e025e5ad7edab96fd5b45529e01a62078d6905e5
siri <51947876+siridamineni@users.noreply.github.com>
2024-02-06 20:32:47 +0000

    [LeetCode Sync] Runtime - 50 ms (94.99%), Memory - 51.7 MB (34.87%)

---
+++ my-folder/0101-symmetric-tree/solution.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var isMirror = function(left, right) {
+    if(!left && !right){
+        return true;
+    }
+    if(!left || !right || left.val !== right.val){
+        return false;
+    }
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+}
+var isSymmetric = function(root) {
+    if(!root){
+        return true;
+    }
+    return isMirror(root.left, root.right);
+};

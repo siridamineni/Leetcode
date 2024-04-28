c6ac4e318859a4d1cfd9fb44310917e3cbc2d053
siri <51947876+siridamineni@users.noreply.github.com>
2024-04-28 15:37:50 +0000

    [LeetCode Sync] Runtime - 57 ms (26.73%), Memory - 49.1 MB (7.80%)

---
+++ my-folder/2796-allow-one-function-call/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var cal = false;
+    return function(...args){
+        if(!cal){
+            cal = true;
+            return fn(...args);
+        }
+    }
+
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
